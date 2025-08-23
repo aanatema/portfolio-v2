f917348be867acba93483db687a586bb0f8e02d9
aanatema <romane.boireau@gmail.com>
2025-08-23 12:49:59 +0200

    change name of component

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,12 +1,11 @@
 import { HomepageAccordion } from "@/app/components/HomepageAccordion";
 import HomepageRedirection from "@/app/components/HomepageRedirection";
 
-export default function HomepageLayout() {
-    return (
-        <>
-            <HomepageRedirection />
-            <HomepageAccordion />
-        </>
-        
-    )
+export default function Homepage() {
+	return (
+		<>
+			<HomepageRedirection />
+			<HomepageAccordion />
+		</>
+	);
 }
