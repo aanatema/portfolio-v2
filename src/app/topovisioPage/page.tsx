8af4246f479af7dbb817233bd9c4e1f0076409cd
aanatema <romane.boireau@gmail.com>
2025-08-23 12:49:36 +0200

    create topovisio page

---
+++ src/app/topovisioPage/page.tsx
@@ -0,0 +1,18 @@
+import HomepageRedirection from "@/app/components/HomepageRedirection";
+import { FolderOpen } from "lucide-react";
+
+export default function TopovisioLayout() {
+	return (
+		<>
+			<HomepageRedirection />
+			<main className='flex flex-col gap-2 justify-center mt-10 pr-6 pl-6'>
+				<header className='flex flex-1 items-start justify-between border-b'>
+					<FolderOpen className='h-8 w-8 shrink-0 stroke-1' />
+					<h1 className='text-left text-md font-medium tracking-wide uppercase self-center'>
+						Topovisio
+					</h1>
+				</header>
+			</main>
+		</>
+	);
+}
