c4636b7298217621f5e58fe4c6fc27f430731091
aanatema <romane.boireau@gmail.com>
2025-08-24 14:19:55 +0200

    add elements in about section of homepage

--- src/app/components/HomepageAccordion.tsx
+++ src/app/components/HomepageAccordion.tsx
@@ -7,20 +7,17 @@ import {
 import Image from "next/image";
 import pp from "../../../public/pp.jpg";
 import abtasty from "../../../public/abtasty.png";
-import {
-	ChevronDown,
-	FileText,
-	FolderIcon,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { ChevronDown, FileText, FolderIcon } from "lucide-react";
 import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
 
 export function HomepageAccordion() {
 	return (
 		<Accordion
 			type='single'
 			collapsible
-			className='w-full mt-10 pr-6 pl-6'
+			className='w-full mt-5 pr-6 pl-6'
 			defaultValue='item-1'>
 			<AccordionItem value='item-1'>
 				<AccordionTrigger>À propos</AccordionTrigger>
@@ -29,36 +26,81 @@ export function HomepageAccordion() {
 						<Image
 							src={pp}
 							alt='Romane Boireau photo'
-							className='rounded-sm h-38 w-38 md:h-76 md:w-76'
+							className='rounded-sm h-35 w-35 md:h-76 md:w-76'
 						/>
 					</section>
 
-					<section className='mb-5'>
+					<section className='mb-2'>
 						<header className='font-bold'>Intro</header>
 						<p className='tracking-[3] text-start uppercase'>
 							[Open to work] / [Nantes]
 						</p>
 						<br />
 						<p>
-							Diplômée après 2 ans de formation dont 12 mois en
-							alternance. Je cherche un poste à temps plein.
-							Orientée front mais toujours volontaire pour élargir
-							le champ de mes compétences.
+							Après deux ans de formation, dont douze mois en
+							alternance, je cherche a intégrer une équipe
+							dynamique et bienveillante. J&apos;ai une préférence
+							marquée pour le front mais suis également ouverte a
+							développer mes compétences en back.
+							<br />
 						</p>
+					</section>
+					<section className='flex flex-row gap-2 justify-end'>
+						<Badge
+							asChild
+							variant='pink'>
+							<Link href='https://drive.google.com/file/d/12MQH5S-7LpIIaAfaXz9vK91MYIKKjI1N/view?usp=drive_link'>
+								CV
+							</Link>
+						</Badge>
+						<Badge
+							asChild
+							variant='blue'>
+							<Link
+								href='https://linkedin.com/in/romane-boireau'
+								className='uppercase'>
+								Linkedin
+							</Link>
+						</Badge>
+						<Badge
+							asChild
+							variant='yellow'>
+							<Link
+								href='https://github.com/aanatema'
+								className='uppercase'>
+								GitHub
+							</Link>
+						</Badge>
+					</section>
+					<header className='font-bold'>Frontend</header>
+					<section className='flex flex-row gap-2 uppercase h-5 text-sm overflow-scroll h-5'>
+						<p>React</p>
+						<Separator orientation='vertical' />
+						<p>Next</p>
+						<Separator orientation='vertical' />
+						<p>Typescript</p>
+						<Separator orientation='vertical' />
+						<p>Tailwind</p>
+						<Separator orientation='vertical' />
+						<p>Shadcn/ui</p>
+					</section>
+					<header className='font-bold'>Backend</header>
+					<section className='flex flex-row gap-2 mb-2 uppercase overflow-scroll h-5'>
+						<p>Node</p>
+						<Separator orientation='vertical' />
+						<p>Express</p>
+						<Separator orientation='vertical' />
+						<p>Typescript</p>
+						<Separator orientation='vertical' />
+						<p>PostgreSQL</p>
+						<Separator orientation='vertical' />
+						<p>Prisma</p>
 					</section>
 					<Link
 						href='/aboutPage'
 						className='flex justify-center'>
 						<ChevronDown stroke='grey' />
 					</Link>
-					<section className='flex flex-row gap-2 justify-between'>
-						<Link href='https://drive.google.com/file/d/1TlAS29rITy7vED9qf4F7MxVS0rXzEIcy/view?usp=drive_link'>
-							<Button>CV</Button>
-						</Link>
-						<Link href='https://github.com/aanatema'>
-							<Button className='uppercase'>Github</Button>
-						</Link>
-					</section>
 				</AccordionContent>
 			</AccordionItem>
 			<AccordionItem value='item-2'>
@@ -67,8 +109,11 @@ export function HomepageAccordion() {
 					<Link
 						href='/biomePage'
 						className='flex col-2 gap-5'>
-						<FileText className='h-8 w-8 shrink-0 stroke-1' />
-						<p className='flex self-center'>Biome</p>
+						<FileText className='h-8 w-8 shrink-0 stroke-1 self-center' />
+						<div className='flex flex-col'>
+							<p className='uppercase'>Biome</p>
+							<p>Projet de diplôme</p>
+						</div>
 					</Link>
 					<Link
 						href='/portfolioPage'
