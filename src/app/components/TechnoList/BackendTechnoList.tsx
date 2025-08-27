8de88332e96ce775bea678fd4580789ef0fc3505
aanatema <romane.boireau@gmail.com>
2025-08-27 15:34:17 +0200

    create components for technos

---
+++ src/app/components/TechnoList/BackendTechnoList.tsx
@@ -0,0 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+
+export default function BackendTechnoList() {
+    return (
+		<>
+			<header className='font-bold'>Backend</header>
+			<section className='flex flex-row gap-2 mb-2 uppercase overflow-scroll h-5'>
+				<p>Node</p>
+				<Separator orientation='vertical' />
+				<p>Express</p>
+				<Separator orientation='vertical' />
+				<p>Typescript</p>
+				<Separator orientation='vertical' />
+				<p>PostgreSQL</p>
+				<Separator orientation='vertical' />
+				<p>Prisma</p>
+			</section>
+		</>
+	);
+}
---
+++ src/app/components/TechnoList/FrontendTechnoList.tsx
@@ -0,0 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+
+export default function FrontendTechnoList() {
+    return (
+		<>
+			<header className='font-bold'>Frontend</header>
+			<section className='flex flex-row gap-2 uppercase h-5 text-sm overflow-scroll h-5'>
+				<p>React</p>
+				<Separator orientation='vertical' />
+				<p>Next</p>
+				<Separator orientation='vertical' />
+				<p>Typescript</p>
+				<Separator orientation='vertical' />
+				<p>Tailwind</p>
+				<Separator orientation='vertical' />
+				<p>Shadcn/ui</p>
+			</section>
+		</>
+	);
+}
