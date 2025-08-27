5a901cf7962d81c9655ba68c9c798f4c24055cd4
aanatema <romane.boireau@gmail.com>
2025-08-27 15:47:51 +0200

    Create components for the projects buttons

---
+++ src/app/components/Buttons/SchoolProjects.tsx
@@ -0,0 +1,17 @@
+import { FolderIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function SchoolProjects() {
+	return (
+		<>
+			<Link
+				href='/schoolPage'
+				className='flex col-2 gap-5'>
+				<FolderIcon className='h-8 w-8 shrink-0 stroke-1' />
+				<p className='flex self-center tracking-[3] uppercase'>
+					Projets d&apos;école
+				</p>
+			</Link>
+		</>
+	);
+}
