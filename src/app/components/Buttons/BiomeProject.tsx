2d4345b28da9fd0bfc0792b5ead6aaa1d7aed5fa
aanatema <romane.boireau@gmail.com>
2025-09-02 11:25:03 +0200

    button to biome page

---
+++ src/app/components/Buttons/BiomeProject.tsx
@@ -0,0 +1,18 @@
+import { FileText } from "lucide-react";
+import Link from "next/link";
+
+export default function BiomeProject() {
+	return (
+		<>
+			<Link
+				href='/biomePage'
+				className='flex col-2 gap-5'>
+				<FileText className='h-8 w-8 shrink-0 stroke-1 stroke-yellow' />
+				<div className='flex flex-col'>
+					<p className='uppercase tracking-[3]'>Biome</p>
+					<p>Projet de diplôme</p>
+				</div>
+			</Link>
+		</>
+	);
+}
