584a149c064dca73a01392b667076318f5317f96
aanatema <romane.boireau@gmail.com>
2025-09-02 11:35:25 +0200

    biome logo

---
+++ src/app/components/Images/BiomeLogo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import biome from "../../../../public/portfolio assets/biomeLogo.svg"
+
+export default function BiomeLogo() {
+	return (
+		<>
+			<Image
+				src={biome}
+				alt='Biome logo'
+				className='h-25 w-50 md:h-40 md:w-40'
+			/>
+		</>
+	);
+}
