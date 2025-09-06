6022bd7dc433d2737d2c9d71bf28756320793478
aanatema <romane.boireau@gmail.com>
2025-09-06 11:00:31 +0200

    change in style of homepage redirection text

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -3,9 +3,9 @@ import { NavigationMenu } from "./NavigationMenu";
 
 export default function NavigationBar() {
 	return (
-		<nav className='flex cols-2 justify-between items-center pt-3 pb-3 pr-5 pl-5 border-b border-foreground bg-background'>
+		<nav className='flex cols-2 justify-between items-center m-6 border-b border-foreground bg-background'>
 			<Link href='homepage'>
-				<h1 className='text-xl font-bold uppercase tracking-wide'>
+				<h1 className='text-xl font-bold uppercase tracking-[3]'>
 					Romane Boireau
 				</h1>
 			</Link>
