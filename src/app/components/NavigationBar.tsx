5c57e17689d998fb7e0a3c65592939058d821a02
aanatema <romane.boireau@gmail.com>
2025-09-06 11:44:18 +0200

    add some bottom padding

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -3,7 +3,7 @@ import { NavigationMenu } from "./NavigationMenu";
 
 export default function NavigationBar() {
 	return (
-		<nav className='flex cols-2 justify-between items-center m-6 border-b border-foreground bg-background'>
+		<nav className='flex cols-2 justify-between items-center m-6 pb-1 border-b border-foreground bg-background'>
 			<Link href='homepage'>
 				<h1 className='text-xl font-bold uppercase tracking-[3]'>
 					Romane Boireau
