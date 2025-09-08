dd20b471a3a849bb3efdb063ca2e4ab4b32b7925
aanatema <romane.boireau@gmail.com>
2025-09-08 19:04:13 +0200

    school projects cards content list

---
+++ src/app/components/SchoolProjectCardList.tsx
@@ -0,0 +1,40 @@
+export const schoolProjects = [
+	{
+		title: "Portfolio [Première version]",
+		description:
+			"Projet ayant pour but de présenter les projets réalisés pendant la formation.",
+		techs: ["React", "Bootstrap", "Email JS"],
+		link: {
+			href: "https://web.archive.org/web/20250314091311/http://romane-boireau.fr",
+			label: "Archive du site",
+		},
+	},
+	{
+		title: "Spots & Potes [App Android]",
+		description:
+			"Application mobile Android reliée à une base de données en utilisant Android Studio. Découverte de l’architecture MVVM.",
+		techs: ["AndroidStudio", "Kotlin", "GradleGo", "MySQL", "RetroFit"],
+	},
+	{
+		title: "Consonea [Projet Fullstack]",
+		description:
+			"Création d’un site e-commerce avec API et base de données fonctionnelles.",
+		techs: ["React", "Bootstrap", "Express", "MySQL", "Node"],
+	},
+	{
+		title: "Kind Glyphs [Chrome Extension]",
+		description:
+			"Extension navigateur qui censure les mots choisis pour une navigation plus agréable.",
+		techs: ["JavaScript", "HTML", "CSS"],
+	},
+	{
+		title: "Pico Resto [Mini Jeu]",
+		description:
+			"Mini jeu où l’on prépare et sert les commandes des clients.",
+		techs: ["Pico-8", "Lua"],
+		link: {
+			href: "https://www.lexaloffle.com/bbs/?tid=53280",
+			label: "Jouer au jeu",
+		},
+	},
+];
