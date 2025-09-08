790abd4152c39978d3ddaa5a66c58b371b60f7a2
aanatema <romane.boireau@gmail.com>
2025-09-08 19:04:33 +0200

    refacto to create reusable components

--- src/app/schoolProjectsPage/page.tsx
+++ src/app/schoolProjectsPage/page.tsx
@@ -1,17 +1,7 @@
-
-import { Badge } from "@/components/ui/badge";
 import { FolderOpen } from "lucide-react";
 import NavigationBar from "../components/NavigationBar";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import GithubBadge from "../components/Buttons/Badges/GithubBadge";
-import Link from "next/link";
+import ProjectCard from "../components/ProjectCard"; 
+import { schoolProjects } from "../components/SchoolProjectCardList";
 
 export default function SchoolProjectsPage() {
 	return (
@@ -25,118 +15,13 @@ export default function SchoolProjectsPage() {
 					</h1>
 				</header>
 
-				<section className=''>
-					<Card>
-						<CardHeader>
-							<CardTitle>Portfolio [Première version]</CardTitle>
-							<CardDescription>
-								Projet ayant pour but de présenter les projets
-								réalisés pendant la formation.
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<Badge variant='pink'>React</Badge>
-							<Badge variant='yellow'>Bootstrap</Badge>
-							<Badge variant='blue'>Email JS</Badge>
-						</CardContent>
-						<CardFooter>
-							<GithubBadge />
-						</CardFooter>
-					</Card>
-					<div>
-						<Card>
-							<CardHeader>
-								<CardTitle>
-									Spots & Potes [App Android]
-								</CardTitle>
-								<CardDescription>
-									Projet d&apos;application mobile Android
-									reliée à une base de données en utilisant
-									Android Studio. Découverte et application de
-									l&apos;architecture MVVM.
-								</CardDescription>
-							</CardHeader>
-							<CardContent>
-								<Badge variant='pink'>Android Studio</Badge>
-								<Badge variant='yellow'>Kotlin</Badge>
-								<Badge variant='blue'>GradleGo</Badge>
-								<Badge variant='pink'>MySQL</Badge>
-								<Badge variant='yellow'>RetroFit</Badge>
-							</CardContent>
-							<CardFooter>
-								<GithubBadge />
-							</CardFooter>
-						</Card>
-					</div>
-					<div>
-						<Card>
-							<CardHeader>
-								<CardTitle>Consonea [Projet Fullstack]</CardTitle>
-								<CardDescription>
-									Création d&apos;un site e-commerce avec API
-									et base de données fonctionnelles.
-								</CardDescription>
-							</CardHeader>
-							<CardContent>
-								<Badge variant='pink'>React</Badge>
-								<Badge variant='yellow'>Bootstrap</Badge>
-								<Badge variant='blue'>Express</Badge>
-								<Badge variant='pink'>MySQL</Badge>
-								<Badge variant='yellow'>Node</Badge>
-							</CardContent>
-							<CardFooter>
-								<GithubBadge />
-							</CardFooter>
-						</Card>
-					</div>
-					<div>
-						<Card>
-							<CardHeader>
-								<CardTitle>
-									Kind Glyphs [Chrome Extension]
-								</CardTitle>
-								<CardDescription>
-									Extension navigateur qui censure les mots
-									choisis pour une navigation plus agréable.
-								</CardDescription>
-							</CardHeader>
-							<CardContent>
-								<Badge variant='pink'>JavaScript</Badge>
-								<Badge variant='yellow'>Html</Badge>
-								<Badge variant='blue'>Css</Badge>
-							</CardContent>
-							<CardFooter>
-								<GithubBadge />
-							</CardFooter>
-						</Card>
-					</div>
-					<div>
-						<Card>
-							<CardHeader>
-								<CardTitle>Pico Resto [Mini Jeu]</CardTitle>
-								<CardDescription>
-									Mini jeu où l&apos;on prépare et sers les
-									commandes des clients.
-								</CardDescription>
-							</CardHeader>
-							<CardContent>
-								<Badge variant='pink'>Pico-8</Badge>
-								<Badge variant='yellow'>Lua</Badge>
-							</CardContent>
-							<CardFooter>
-								<Badge
-									asChild
-									variant='blue'>
-									<Link
-										href='https://www.lexaloffle.com/bbs/?tid=53280'
-										className='uppercase'>
-										Jouer au jeu
-									</Link>
-								</Badge>
-								<GithubBadge />
-							</CardFooter>
-						</Card>
-					</div>
+				<section className='flex flex-col gap-4'>
+					{schoolProjects.map((project, index) => (
+						<ProjectCard
+							key={index}
+							{...project}
+						/>
+					))}
 				</section>
 			</main>
 		</>
