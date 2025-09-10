cd5781792668388243df100e8ad57a92ed6e975f
aanatema <romane.boireau@gmail.com>
2025-09-10 10:41:01 +0200

    minor fix

--- src/app/components/Images/AbtastyLogo.tsx
+++ src/app/components/Images/AbtastyLogo.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 export default function AbtastyLogo() {
 	return (
 		<>
-			<Link href='https://www.abtasty.com/fr/'>
-				<section className='bg-abt-blue p-2 rounded-sm self-center'>
+			<section className='bg-abt-blue p-2 rounded-sm self-center'>
+				<Link href='https://www.abtasty.com/fr/'>
 					<Image
 						src={abtasty}
 						alt='AB Tasty logo'
 						className='h-7 w-32 md:h-40 md:w-40'
 					/>
-				</section>
-			</Link>
+				</Link>
+			</section>
 		</>
 	);
 }
