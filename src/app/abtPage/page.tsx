e795a3ae26829406571b68b8d26412c6f0661935
aanatema <romane.boireau@gmail.com>
2025-09-10 10:32:55 +0200

    create page for abt missions

---
+++ src/app/abtPage/page.tsx
@@ -0,0 +1,39 @@
+import { FolderOpen } from "lucide-react";
+import AbtastyLogo from "../components/Images/AbtastyLogo";
+import NavigationBar from "../components/NavigationBar";
+import { abtMissionsList } from "../components/AbtMissionsList";
+import AbtMissions from "../components/AbtMissions";
+import AbtTechnoList from "../components/TechnoList/AbtTechnoList";
+import { Separator } from "@/components/ui/separator";
+import References from "../components/References";
+import ReferencesBadge from "../components/Buttons/Badges/ReferencesBadge";
+
+export default function AbtPage() {
+	return (
+		<>
+			<NavigationBar />
+			<main className='flex flex-col gap-2 justify-center mt-10 pr-6 pl-6 text-justify'>
+				<header className='flex flex-1 items-start justify-between border-b border-foreground pb-1'>
+					<FolderOpen className='h-8 w-8 shrink-0 stroke-1' />
+					<h1 className='text-left text-md font-medium tracking-wide uppercase self-center'>
+						Missions AB Tasty
+					</h1>
+				</header>
+				<AbtTechnoList />
+				<div className="self-center mb-5">
+					<ReferencesBadge />
+				</div>
+				<Separator />
+				<section className='mt-5'>
+					{abtMissionsList.map((mission, idx) => (
+						<AbtMissions
+							key={idx}
+							{...mission}
+						/>
+					))}
+				</section>
+				<References />
+			</main>
+		</>
+	);
+}
