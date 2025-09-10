09e879b561ac4e51afa7f4ce812c76d20b6b3da8
aanatema <romane.boireau@gmail.com>
2025-09-10 10:33:54 +0200

    updates on logo and abt techno list

--- src/app/components/Images/AbtastyLogo.tsx
+++ src/app/components/Images/AbtastyLogo.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import abtasty from "../../../../public/abtasty.png";
-
+import Link from "next/link";
 
 export default function AbtastyLogo() {
-    return (
+	return (
 		<>
-			<section className='bg-abt-blue p-2 rounded-sm self-center'>
-				<Image
-					src={abtasty}
-					alt='AB Tasty logo'
-					className='h-7 w-32 md:h-40 md:w-40'
-				/>
-			</section>
+			<Link href='https://www.abtasty.com/fr/'>
+				<section className='bg-abt-blue p-2 rounded-sm self-center'>
+					<Image
+						src={abtasty}
+						alt='AB Tasty logo'
+						className='h-7 w-32 md:h-40 md:w-40'
+					/>
+				</section>
+			</Link>
 		</>
 	);
 }
--- src/app/components/TechnoList/AbtTechnoList.tsx
+++ src/app/components/TechnoList/AbtTechnoList.tsx
@@ -3,9 +3,9 @@ import { Separator } from "@/components/ui/separator";
 export default function AbtTechnoList() {
 	return (
 		<>
-			<div className='mt-5 mb-5 flex flex-col gap-4'>
+			<div className='mt-5 mb-5 flex flex-col gap-2'>
 				<header className='font-bold'>Technos</header>
-				<section className='flex flex-row gap-2 uppercase h-5 overflow-scroll'>
+				<section className='flex flex-row gap-2 uppercase h-7 overflow-scroll'>
 					<p>React</p>
 					<Separator orientation='vertical' />
 					<p>Vite</p>
@@ -18,15 +18,17 @@ export default function AbtTechnoList() {
 					<Separator orientation='vertical' />
 					<p>Docker</p>
 					<Separator orientation='vertical' />
-					<p>Gitlab</p>
+					<p>Git</p>
 				</section>
 				<header className='font-bold'>Outils</header>
-				<section className='flex flex-row gap-2 uppercase h-5 overflow-scroll'>
+				<section className='flex flex-row gap-2 uppercase h-7 overflow-scroll'>
 					<p>Jira</p>
 					<Separator orientation='vertical' />
 					<p>Slack</p>
 					<Separator orientation='vertical' />
 					<p>Confluence</p>
+					<Separator orientation='vertical' />
+					<p>Gitlab</p>
 				</section>
 			</div>
 		</>
