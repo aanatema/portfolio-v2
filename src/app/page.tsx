4e6042d38b0150d835057b2686494cf0e7b1e2b5
aanatema <romane.boireau@gmail.com>
2025-09-16 16:40:25 +0200

    remove homepage redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { HomepageAccordion } from "./components/HomepageComponents/HomepageAccordion";
-import HomepageRedirection from "./components/HomepageComponents/HomepageRedirection";
 
 export default function Home() {
 	return (
 		<>
-			<HomepageRedirection />
 			<main className='md:mx-65'>
 				<HomepageAccordion />
 			</main>
