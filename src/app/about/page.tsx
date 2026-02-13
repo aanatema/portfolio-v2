3ff4e2e8373cad5a7a6ea3e22896bc073f718993
aanatema <romane.boireau@gmail.com>
2026-02-13 17:59:59 +0100

    update about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,97 +17,62 @@ export default function AboutPage() {
 					</header>
 					<br />
 					<p className=''>
-						Issue d’un cursus littéraire, j’ai choisi de me
-						réorienter afin de m’aligner avec mon besoin de réaliser
-						des tâches concrètes et utiles. J&apos;ai donc choisi le
-						développement web et c&apos;est avec Ada Tech School que
-						j&apos;ai commencé ma formation pour le titre{" "}
-						<strong>
-							RNCP VI développeur concepteur d&apos;applications
-						</strong>
-						.
-						<br />
-						<br />
-						Mon{" "}
+						Récemment diplômée après deux ans d’études dont 12 mois
+						d’
 						<a
 							href='/abtasty'
 							className='underline font-bold'>
-							alternance
-						</a>{" "}
-						au sein d’
-						<strong>AB Tasty</strong> m’a permis d&apos;atteindre
-						cette recherche d&apos;utilité dans diverses situations
-						réelles : implémenter des fonctionnalités demandées par
-						les clients, documenter et actualiser nos processus
-						internes, améliorer les interfaces pour une utilisation
-						fluidifiée.
-						<br />À la suite de ces 12 mois d&apos;alternance, j’ai
-						obtenu mon <strong> diplôme </strong> avec la
-						présentation de mon projet{" "}
-						<a
-							href='/biome'
-							className='underline'>
-							Biome
+							alternance au sein du SaaS AB Tasty
 						</a>
-						, un carnet de lecture en ligne qui s’affranchit des
-						algorithmes des réseaux sociaux.
-					</p>
-					<br />
-					<br />
-					<header className='font-medium uppercase'>
-						[Ce que j&apos;aime et cherche]
-					</header>
-					<br />
-					<p>
-						J’aime prendre le temps de m’intéresser à ce que font
-						les autres membres de mon équipe pour discuter et
-						apprendre d’eux. Je tiens à avoir une{" "}
-						<strong> vision globale </strong> du / des projets et
-						des enjeux de l’entreprise dans laquelle je suis. Il est
-						important pour moi que les différentes équipes ne soient
-						pas uniquement cantonnées à leurs projets sans
-						compréhension collective.
+						, j’ai choisi de poursuivre sur un Master Frontend afin
+						d’élargir le champ de mes compétences dans ce domaine.
+						<br />
+						C’est dans ce carde que je cherche une{" "}
+						<strong>alternance</strong> de 24 mois pour une rentrée
+						en <strong>Septembre 2026</strong>.
 						<br />
 						<br />
-						J’apprécie beaucoup échanger sur les questions d’
-						<strong>UI/UX </strong>
-						et autres enjeux liés aux différents rôles d’une équipe
-						(QA, PM…), travailler dans une équipe variée où l’on
-						peut prendre le temps d’échanger et de se challenger les
-						uns les autres. J’aime{" "}
-						<strong>apprendre et partager</strong> avec mon équipe
-						mais aussi les autres membres du bureau.
+						Ma première alternance m’a permis d’évoluer au sein
+						d’une équipe agile internationale avec une collaboration
+						régulière avec les équipes, Design, Produit et QA.
+						<br />
+						J’ai pu développer mes compétences en travaillant sur
+						des projets matures, avec leurs propres standards,
+						enjeux et complexités; contribuer au développement et à
+						l’évolution de fonctionnalités dans un environnement
+						<strong> React - Typescript</strong>; participer à
+						l’évolution et à la structuration de l’architecture
+						React.
+						<br />
+						J’ai pris l’initiative de mettre à jour et de créer de
+						la documentation sur les projets qui m’ont été assignés
+						afin de simplifier les contributions et de garder une
+						trâce de leurs évolutions.
+						<br />
+						<br />
+						Pour ma seconde alternance, je souhaite affiner mes
+						connaissances, être challengée et travailler sur des
+						projets variés au sein d’une équipe dynamique aux
+						missions clairement définies.
 					</p>
 					<br />
-					<br />
+
 					<header className='font-medium uppercase'>
 						[Ce que j&apos;apporte]
 					</header>
 					<br />
 					<p>
-						Comme évoqué plus tôt j&apos;aime apprendre et partager
-						ce qui fait de moi quelqu&apos;un de volontaire et animé
-						par l&apos;envie de{" "}
-						<strong> faire avancer les choses</strong>. Mon goût
-						pour le design me permet de porter une attention
-						particulière à l&apos;éxpérience utilisateur et la
-						<strong> qualité des interfaces </strong>lors du
-						développement de fonctionnalités et facilite les
-						échanges avec l&apos;équipe design.
-						<br />
-						<br />
-						Mon passage au sein d&apos;AB Tasty m&apos;a permis de
-						mettre en application mes connaissances sur des{" "}
-						<strong>projets matures et complexes</strong> en
-						m&apos;adaptant aux standards de qualité et processus en
-						vigueur. J&apos;ai pu développer mes compétences,
-						notamment en <strong>React</strong> et{" "}
-						<strong>Typescript </strong> en conditions réelles.
-						<br />
-						<br />
-						J&apos;aime écrire de la <strong>documentation</strong>,
-						une tâche essentielle au développement de chaque projet
-						mais qui est pourtant trop peu appréciée.
+						Une première expérience professionnelle au sein
+						d&apos;un environnement exigeant avec une équipe
+						structurée responsable d&apos;une section précise des
+						services d&apos;AB Tasty. <br />
+						Je suis familière avec le fonctionnement d&apos;une
+						équipe tech, aux rituels agile, à m&apos;adapter aux
+						standards de qualité et processus en vigueur dans les
+						projets et comprends les enjeux de faire partie
+						d&apos;un plus grand ensemble. J&apos;apprends vite et
+						m&apos;investis pleinement pour mon équipe et dans les
+						tâches qui me sont confiées.
 					</p>
 				</section>
 			</main>
